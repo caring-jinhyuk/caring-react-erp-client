8882b0f173fb41f5c831eddb378598e56b9ce0d5
caring-jinhyuk <jh@caring.co.kr>
2022-07-27 17:53:51 +0900

    baseURL 수정, 홈헤더 추가.

--- src/routes/headerRoutes.tsx
+++ src/routes/headerRoutes.tsx
@@ -3,7 +3,6 @@ import DefaultHeader from '../pages/common/Headers/DefaultHeader';
 import { authPages, homepages } from '../menus/menu';
 
 const headers = [
-	{ path: homepages.home.path, element: null, exact: true },
 	{ path: authPages.signIn.path, element: null, exact: true },
 	{ path: authPages.signUp.path, element: null, exact: true },
 	{
