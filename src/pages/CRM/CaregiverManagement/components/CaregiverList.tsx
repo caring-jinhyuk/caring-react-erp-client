af6eea01c1481c899f0a2d79cdf97f5e949bb2b8
donghyuk.park <sebastian.park15@gmail.com>
2022-08-03 18:02:27 +0900

    feat : ERP-13 구직 요양보호사 페이징추가

--- src/pages/CRM/CaregiverManagement/components/CaregiverList.tsx
+++ src/pages/CRM/CaregiverManagement/components/CaregiverList.tsx
@@ -1,15 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Caregiver } from '../../../../services/openApi';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { caregiverModal, selectCaregiver } from '../CaregiverContainer';
 import { useGetCaregiverList } from '../../../../quries/useCaregiverList';
 import { caregiverSearchParam } from '../CaregiverListHeader';
+import PaginationButtons from '../../../../components/PaginationButtons';
 
 const CaregiverList: FC = () => {
 	const setCaregiver = useSetRecoilState(selectCaregiver);
 	const setOpen = useSetRecoilState(caregiverModal);
 	const param = useRecoilValue(caregiverSearchParam);
-	const caregivers = useGetCaregiverList(param.keyword, param.search);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [perPage, setPerPage] = useState(10);
+	const caregivers = useGetCaregiverList(param.keyword, param.search, currentPage, perPage);
 
 	const openDetail = (item: Caregiver) => {
 		setCaregiver(item);
@@ -30,8 +33,8 @@ const CaregiverList: FC = () => {
 					</tr>
 				</thead>
 				<tbody>
-					{caregivers.data &&
-						caregivers.data.map((item) => (
+					{caregivers.contents?.content &&
+						caregivers.contents.content.map((item) => (
 							// eslint-disable-next-line react/jsx-key
 							<tr key={item.id} onClick={() => openDetail(item)}>
 								<td>{item.name}</td>
@@ -44,6 +47,14 @@ const CaregiverList: FC = () => {
 						))}
 				</tbody>
 			</table>
+			<PaginationButtons
+				data={new Array(caregivers.contents?.totalElements)}
+				label='구직 요양보호사'
+				setCurrentPage={setCurrentPage}
+				currentPage={currentPage}
+				perPage={perPage}
+				setPerPage={setPerPage}
+			/>
 		</>
 	);
 };
