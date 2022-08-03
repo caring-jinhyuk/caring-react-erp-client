2173d83ad1af1d041d39058058304dd6452f301f
donghyuk.park <sebastian.park15@gmail.com>
2022-08-03 17:27:41 +0900

    feat : ERP-13 구직 요양보호사 페이지 수정

--- src/pages/CRM/CaregiverManagement/components/CaregiverDetail.tsx
+++ src/pages/CRM/CaregiverManagement/components/CaregiverDetail.tsx
@@ -19,6 +19,7 @@ import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { caregiverModal, selectCaregiver } from '../CaregiverContainer';
 import { caregiverSearchParam } from '../CaregiverListHeader';
 import { useQueryClient } from '@tanstack/react-query';
+import exp from 'constants';
 
 export type CaregiverForm = {
 	address?: string;
@@ -30,7 +31,7 @@ export type CaregiverForm = {
 	covid?: boolean;
 	createdAt?: string;
 	dementia?: boolean;
-	gender?: boolean;
+	gender: string;
 	hopeArea?: string;
 	id?: number;
 	information?: string;
@@ -49,26 +50,44 @@ export type CaregiverForm = {
 	takerProgress?: string;
 	town?: string;
 	ward?: string;
-	work_kinds: boolean[];
+	work_kinds: string[];
 	writer?: string;
 	year?: number;
+	hope_city1?: string;
+	hope_city2?: string;
+	hope_city3?: string;
+	hope_ward1?: string;
+	hope_ward2?: string;
+	hope_ward3?: string;
+	hope_area?: HopeArea;
+};
+
+export type HopeArea = {
+	hope_city1: string;
+	hope_city2: string;
+	hope_city3: string;
+	hope_ward1: string;
+	hope_ward2: string;
+	hope_ward3: string;
 };
 
 const CaregiverDetail: FC = () => {
 	const caregiver = useRecoilValue(selectCaregiver);
+
 	const formCaregiver: CaregiverForm = {
 		id: caregiver.id,
+		black: caregiver.black,
 		address: caregiver.address,
 		career: caregiver.career,
 		certificate: caregiver.certificate,
 		city: caregiver.city,
 		covid: caregiver.covid,
 		dementia: caregiver.dementia,
-		gender: caregiver.gender,
+		gender: caregiver.gender ? '여자' : '남자',
 		hopeArea: caregiver.hopeArea,
 		information:
 			caregiver.information ||
-			"'이력/경력사항 : \\n' + '요보사가 원하는 사항 : \\n' + '면접 특이사항 : '",
+			'이력/경력사항 : \n' + '요보사가 원하는 사항 : \n' + '면접 특이사항 : ',
 		takerProgress:
 			caregiver.takerProgress ||
 			'1. 수급자 지역 : \n' + '2. 수급자 전화번호: \n' + '3. 수급자 날짜별 진행 상황 : ',
@@ -78,6 +97,9 @@ const CaregiverDetail: FC = () => {
 		work_kinds: convertWorkKind(caregiver.work_kinds),
 		town: caregiver.town,
 		ward: caregiver.ward,
+		hope_area: convertHopeArea(caregiver.hopeArea),
+		year: caregiver.year,
+		createdAt: caregiver.createdAt,
 	};
 	const [open, setOpen] = useRecoilState(caregiverModal);
 	const setSearchParam = useSetRecoilState(caregiverSearchParam);
@@ -88,8 +110,37 @@ const CaregiverDetail: FC = () => {
 		initialValues: formCaregiver,
 		// eslint-disable-next-line no-unused-vars
 		onSubmit: async (values) => {
-			alert(JSON.stringify(values, null, 2));
-			//await saveCaregiver(values);
+			//alert(JSON.stringify(values, null, 2));
+			const submitValues: Caregiver = {
+				id: values.id,
+				black: values.black,
+				address: values.address,
+				career: values.career,
+				certificate: values.certificate,
+				city: values.city,
+				covid: values.covid,
+				dementia: values.dementia,
+				gender: values.gender !== '남자',
+				information: values.information,
+				takerProgress: values.takerProgress,
+				name: values.name,
+				phone: values.phone,
+				town: values.town,
+				ward: values.ward,
+				prefer_gender: makePreferGenderString(values.prefer_gender),
+				work_kinds: makeWorkKindString(values.work_kinds),
+				hopeArea: makeHomeArea(
+					values.hope_area!.hope_city1,
+					values.hope_area!.hope_city2,
+					values.hope_area!.hope_city3,
+					values.hope_area!.hope_ward1,
+					values.hope_area!.hope_ward2,
+					values.hope_area!.hope_ward3,
+				),
+				year: values.year,
+				createdAt: values.createdAt,
+			};
+			await saveCaregiver(submitValues);
 		},
 	});
 
@@ -127,15 +178,103 @@ const CaregiverDetail: FC = () => {
 
 	function convertWorkKind(value?: string) {
 		if (value === null) {
-			return [false, false, false, false];
+			return [];
 		} else {
-			return [
-				value!.includes('방문요양'),
-				value!.includes('방문목욕'),
-				value!.includes('입주요양'),
-				value!.includes('요양시설'),
-			];
+			const arrWorkKind = [];
+			if (value?.includes('방문요양')) {
+				arrWorkKind.push('방문요양');
+			} else if (value?.includes('방문목욕')) {
+				arrWorkKind.push('방문목욕');
+			} else if (value?.includes('입주요양')) {
+				arrWorkKind.push('입주요양');
+			} else if (value?.includes('요양시설')) {
+				arrWorkKind.push('요양시설');
+			}
+
+			return arrWorkKind;
 		}
+	}
+
+	function convertHopeArea(value?: string) {
+		if (value == null) {
+			const emptyHopeArea: HopeArea = {
+				hope_city1: '',
+				hope_city2: '',
+				hope_city3: '',
+				hope_ward1: '',
+				hope_ward2: '',
+				hope_ward3: '',
+			};
+			return emptyHopeArea;
+		} else {
+			const homeValue = value?.split(',');
+			const hopeTown = ['', '', ''];
+			const hopeWard = ['', '', ''];
+			for (let i = 0; i < homeValue.length; i++) {
+				hopeTown[i] = homeValue[i].substr(0, 2);
+				hopeWard[i] = homeValue[i].substring(3);
+			}
+
+			const hopeAreaValue: HopeArea = {
+				hope_city1: hopeTown[0],
+				hope_city2: hopeTown[1],
+				hope_city3: hopeTown[2],
+				hope_ward1: hopeWard[0],
+				hope_ward2: hopeWard[1],
+				hope_ward3: hopeWard[2],
+			};
+
+			return hopeAreaValue;
+		}
+	}
+
+	function makePreferGenderString(value?: string[]) {
+		let stringPreferGender = '';
+		if (value?.includes('남자')) {
+			stringPreferGender += '남자 ';
+		} else if (value?.includes('여자')) {
+			stringPreferGender += '여자';
+		}
+		return stringPreferGender;
+	}
+
+	function makeWorkKindString(value?: string[]) {
+		let stringWorkKind = '';
+		if (value?.includes('방문요양')) {
+			stringWorkKind += '방문요양 ';
+		} else if (value?.includes('방문목욕')) {
+			stringWorkKind += '방문목욕 ';
+		} else if (value?.includes('입주요양')) {
+			stringWorkKind += '입주요양 ';
+		} else if (value?.includes('요양시설')) {
+			stringWorkKind += '요양시설 ';
+		}
+		return stringWorkKind;
+	}
+
+	function makeHomeArea(
+		city1?: string,
+		city2?: string,
+		city3?: string,
+		warn1?: string,
+		warn2?: string,
+		ward3?: string,
+	) {
+		let stringHomeArea = '';
+
+		if (city1 !== '') {
+			stringHomeArea += city1;
+			stringHomeArea += ' ' + warn1 + ',';
+		}
+		if (city2 !== '') {
+			stringHomeArea += city2;
+			stringHomeArea += ' ' + warn2 + ',';
+		}
+		if (city3 !== '') {
+			stringHomeArea += city3;
+			stringHomeArea += ' ' + ward3 + ',';
+		}
+		return stringHomeArea;
 	}
 
 	return (
@@ -176,10 +315,24 @@ const CaregiverDetail: FC = () => {
 							<FormGroup id='gender' className='mb-3' label='성별'>
 								<div className='row'>
 									<div className='col-6'>
-										<Checks type='radio' name='gender' label='남' onChange={formik.handleChange} />
+										<Checks
+											type='radio'
+											value='남자'
+											name='gender'
+											label='남자'
+											checked={formik.values.gender === '남자'}
+											onChange={formik.handleChange}
+										/>
 									</div>
 									<div className='col-6'>
-										<Checks type='radio' name='gender' label='여' onChange={formik.handleChange} />
+										<Checks
+											type='radio'
+											value='여자'
+											name='gender'
+											label='여자'
+											checked={formik.values.gender === '여자'}
+											onChange={formik.handleChange}
+										/>
 									</div>
 								</div>
 							</FormGroup>
@@ -217,24 +370,24 @@ const CaregiverDetail: FC = () => {
 						<CardHeader>희망지역</CardHeader>
 						<CardBody>
 							<AddressPicker
-								cityId={'city'}
-								cityValue={formik.values.city}
-								wardId={'ward'}
-								wardValue={formik.values.ward}
+								cityId={'hope_area.hope_city1'}
+								cityValue={formik.values.hope_area!.hope_city1}
+								wardId={'hope_area.hope_ward1'}
+								wardValue={formik.values.hope_area!.hope_ward1}
 								onChange={formik.handleChange}
 							/>
 							<AddressPicker
-								cityId={'city'}
-								cityValue={formik.values.city}
-								wardId={'ward'}
-								wardValue={formik.values.ward}
+								cityId={'hope_area.hope_city2'}
+								cityValue={formik.values.hope_area!.hope_city2}
+								wardId={'hope_area.hope_ward2'}
+								wardValue={formik.values.hope_area!.hope_ward2}
 								onChange={formik.handleChange}
 							/>
 							<AddressPicker
-								cityId={'city'}
-								cityValue={formik.values.city}
-								wardId={'ward'}
-								wardValue={formik.values.ward}
+								cityId={'hope_area.hope_city3'}
+								cityValue={formik.values.hope_area!.hope_city3}
+								wardId={'hope_area.hope_ward3'}
+								wardValue={formik.values.hope_area!.hope_ward3}
 								onChange={formik.handleChange}
 							/>
 							<FormGroup id='information' label='요양보호사의 정보'>
@@ -252,18 +405,42 @@ const CaregiverDetail: FC = () => {
 							<FormGroup id='work_kinds' label='일자리 종류'>
 								<div className='row'>
 									<div className='col-6'>
-										<Checks name='work_kinds' label='방문요양' />
+										<Checks
+											name='work_kinds'
+											value={'방문요양'}
+											label='방문요양'
+											checked={formik.values.work_kinds?.includes('방문요양')}
+											onChange={formik.handleChange}
+										/>
 									</div>
 									<div className='col-6'>
-										<Checks name='work_kinds' label='방문목욕' />
+										<Checks
+											name='work_kinds'
+											value={'방문목욕'}
+											label='방문목욕'
+											checked={formik.values.work_kinds?.includes('방문목욕')}
+											onChange={formik.handleChange}
+										/>
 									</div>
 								</div>
 								<div className='row'>
 									<div className='col-6'>
-										<Checks name='work_kinds' label='입주요양' />
+										<Checks
+											name='work_kinds'
+											value={'입주요양'}
+											label='입주요양'
+											checked={formik.values.work_kinds?.includes('입주요양')}
+											onChange={formik.handleChange}
+										/>
 									</div>
 									<div className='col-6'>
-										<Checks name='work_kinds' label='요양시설' />
+										<Checks
+											name='work_kinds'
+											value={'요양시설'}
+											label='요양시설'
+											checked={formik.values.work_kinds?.includes('요양시설')}
+											onChange={formik.handleChange}
+										/>
 									</div>
 								</div>
 							</FormGroup>
@@ -327,6 +504,19 @@ const CaregiverDetail: FC = () => {
 											label='동의함'
 											onChange={formik.handleChange}
 											checked={formik.values.privacy!}
+										/>
+									</div>
+								</div>
+							</FormGroup>
+							<FormGroup id='privacy' label='★유의 요보사 (문자발송/매칭에서 제외)'>
+								<div className='row'>
+									<div className='col-6'>
+										<Checks
+											type='switch'
+											name='black'
+											label='유의 요보사 설정'
+											onChange={formik.handleChange}
+											checked={formik.values.black!}
 										/>
 									</div>
 								</div>
