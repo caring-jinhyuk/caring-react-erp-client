714289287c6b99a88e6b98ddfc0a295b794ffdb4
candyman15 <sebastian.park15@gmail.com>
2022-08-09 10:55:18 +0900

    feat : 테스트 커밋

--- src/routes/footerRoutes.tsx
+++ src/routes/footerRoutes.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Footer from '../layout/Footer/Footer';
 import { authPages, homepages } from '../menus/menu';
 
 const footers = [
 	{ path: homepages.home.path, element: null, exact: true },
 	{ path: authPages.signIn.path, element: null, exact: true },
 	{ path: authPages.signUp.path, element: null, exact: true },
-	{ path: '*', element: <Footer /> },
+	{ path: '*', element: null, exact: true },
+	// { path: '*', element: <Footer /> },
 ];
 
 export default footers;
