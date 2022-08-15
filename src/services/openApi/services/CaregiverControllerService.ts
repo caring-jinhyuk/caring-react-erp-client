ce53c615a0f81db7d568339dfcbc29b8130e80df
Jo Jinhyuk <jh@caring.co.kr>
2022-08-16 05:36:39 +0900

    code format 변경.

--- src/services/openApi/services/CaregiverControllerService.ts
+++ src/services/openApi/services/CaregiverControllerService.ts
@@ -10,268 +10,251 @@ import { OpenAPI } from '../core/OpenAPI';
 import { request as __request } from '../core/request';
 
 export class CaregiverControllerService {
+	/**
+	 * getCaregiver
+	 * @param phone phone
+	 * @returns Caregiver OK
+	 * @throws ApiError
+	 */
+	public static getCaregiverUsingGet(phone: string): CancelablePromise<Caregiver> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/caregiver',
+			query: {
+				phone: phone,
+			},
+		});
+	}
 
-    /**
-     * getCaregiver
-     * @param phone phone
-     * @returns Caregiver OK
-     * @throws ApiError
-     */
-    public static getCaregiverUsingGet(
-phone: string,
-): CancelablePromise<Caregiver> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/caregiver',
-            query: {
-                'phone': phone,
-            },
-        });
-    }
+	/**
+	 * saveCaregiver
+	 * @param caregiver caregiver
+	 * @returns Caregiver OK
+	 * @throws ApiError
+	 */
+	public static saveCaregiverUsingPost(caregiver: Caregiver): CancelablePromise<Caregiver> {
+		return __request(OpenAPI, {
+			method: 'POST',
+			url: '/caregiver',
+			body: caregiver,
+		});
+	}
 
-    /**
-     * saveCaregiver
-     * @param caregiver caregiver
-     * @returns Caregiver OK
-     * @throws ApiError
-     */
-    public static saveCaregiverUsingPost(
-caregiver: Caregiver,
-): CancelablePromise<Caregiver> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/caregiver',
-            body: caregiver,
-        });
-    }
+	/**
+	 * deleteCaregiver
+	 * @param id id
+	 * @returns any OK
+	 * @throws ApiError
+	 */
+	public static deleteCaregiverUsingDelete(id: number): CancelablePromise<any> {
+		return __request(OpenAPI, {
+			method: 'DELETE',
+			url: '/caregiver',
+			query: {
+				id: id,
+			},
+		});
+	}
 
-    /**
-     * deleteCaregiver
-     * @param id id
-     * @returns any OK
-     * @throws ApiError
-     */
-    public static deleteCaregiverUsingDelete(
-id: number,
-): CancelablePromise<any> {
-        return __request(OpenAPI, {
-            method: 'DELETE',
-            url: '/caregiver',
-            query: {
-                'id': id,
-            },
-        });
-    }
+	/**
+	 * getCount
+	 * @returns number OK
+	 * @throws ApiError
+	 */
+	public static getCountUsingGet(): CancelablePromise<number> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/caregiver/Count',
+		});
+	}
 
-    /**
-     * getCount
-     * @returns number OK
-     * @throws ApiError
-     */
-    public static getCountUsingGet(): CancelablePromise<number> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/caregiver/Count',
-        });
-    }
+	/**
+	 * getCaregiverMatchingAllList
+	 * @param dementia dementia
+	 * @param gender gender
+	 * @param search search
+	 * @param service service
+	 * @param year year
+	 * @returns Caregiver OK
+	 * @throws ApiError
+	 */
+	public static getCaregiverMatchingAllListUsingGet(
+		dementia?: boolean,
+		gender?: boolean,
+		search?: string,
+		service?: string,
+		year?: number,
+	): CancelablePromise<Array<Caregiver>> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/caregiver/MatchingAllList',
+			query: {
+				dementia: dementia,
+				gender: gender,
+				search: search,
+				service: service,
+				year: year,
+			},
+		});
+	}
 
-    /**
-     * getCaregiverMatchingAllList
-     * @param dementia dementia
-     * @param gender gender
-     * @param search search
-     * @param service service
-     * @param year year
-     * @returns Caregiver OK
-     * @throws ApiError
-     */
-    public static getCaregiverMatchingAllListUsingGet(
-dementia?: boolean,
-gender?: boolean,
-search?: string,
-service?: string,
-year?: number,
-): CancelablePromise<Array<Caregiver>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/caregiver/MatchingAllList',
-            query: {
-                'dementia': dementia,
-                'gender': gender,
-                'search': search,
-                'service': service,
-                'year': year,
-            },
-        });
-    }
+	/**
+	 * getCaregiverMatchingList
+	 * @param dementia dementia
+	 * @param gender gender
+	 * @param page page
+	 * @param search search
+	 * @param service service
+	 * @param size size
+	 * @param year year
+	 * @returns Page_Caregiver_ OK
+	 * @throws ApiError
+	 */
+	public static getCaregiverMatchingListUsingGet(
+		dementia?: boolean,
+		gender?: boolean,
+		page?: number,
+		search?: string,
+		service?: string,
+		size: number = 10,
+		year?: number,
+	): CancelablePromise<Page_Caregiver_> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/caregiver/MatchingList',
+			query: {
+				dementia: dementia,
+				gender: gender,
+				page: page,
+				search: search,
+				service: service,
+				size: size,
+				year: year,
+			},
+		});
+	}
 
-    /**
-     * getCaregiverMatchingList
-     * @param dementia dementia
-     * @param gender gender
-     * @param page page
-     * @param search search
-     * @param service service
-     * @param size size
-     * @param year year
-     * @returns Page_Caregiver_ OK
-     * @throws ApiError
-     */
-    public static getCaregiverMatchingListUsingGet(
-dementia?: boolean,
-gender?: boolean,
-page?: number,
-search?: string,
-service?: string,
-size: number = 10,
-year?: number,
-): CancelablePromise<Page_Caregiver_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/caregiver/MatchingList',
-            query: {
-                'dementia': dementia,
-                'gender': gender,
-                'page': page,
-                'search': search,
-                'service': service,
-                'size': size,
-                'year': year,
-            },
-        });
-    }
+	/**
+	 * getCaregiverAllList
+	 * @param keyword keyword
+	 * @param search search
+	 * @returns Caregiver OK
+	 * @throws ApiError
+	 */
+	public static getCaregiverAllListUsingGet(
+		keyword?: string,
+		search?: string,
+	): CancelablePromise<Array<Caregiver>> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/caregiver/allList',
+			query: {
+				keyword: keyword,
+				search: search,
+			},
+		});
+	}
 
-    /**
-     * getCaregiverAllList
-     * @param keyword keyword
-     * @param search search
-     * @returns Caregiver OK
-     * @throws ApiError
-     */
-    public static getCaregiverAllListUsingGet(
-keyword?: string,
-search?: string,
-): CancelablePromise<Array<Caregiver>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/caregiver/allList',
-            query: {
-                'keyword': keyword,
-                'search': search,
-            },
-        });
-    }
+	/**
+	 * getCityCount
+	 * @param city city
+	 * @returns number OK
+	 * @throws ApiError
+	 */
+	public static getCityCountUsingGet(city: string): CancelablePromise<number> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/caregiver/cityCount',
+			query: {
+				city: city,
+			},
+		});
+	}
 
-    /**
-     * getCityCount
-     * @param city city
-     * @returns number OK
-     * @throws ApiError
-     */
-    public static getCityCountUsingGet(
-city: string,
-): CancelablePromise<number> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/caregiver/cityCount',
-            query: {
-                'city': city,
-            },
-        });
-    }
+	/**
+	 * getCaregiverId
+	 * @param id id
+	 * @returns Caregiver OK
+	 * @throws ApiError
+	 */
+	public static getCaregiverIdUsingGet(id: number): CancelablePromise<Caregiver> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/caregiver/id',
+			query: {
+				id: id,
+			},
+		});
+	}
 
-    /**
-     * getCaregiverId
-     * @param id id
-     * @returns Caregiver OK
-     * @throws ApiError
-     */
-    public static getCaregiverIdUsingGet(
-id: number,
-): CancelablePromise<Caregiver> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/caregiver/id',
-            query: {
-                'id': id,
-            },
-        });
-    }
+	/**
+	 * getCaregiverList
+	 * @param keyword keyword
+	 * @param page page
+	 * @param search search
+	 * @param size size
+	 * @returns Page_Caregiver_ OK
+	 * @throws ApiError
+	 */
+	public static getCaregiverListUsingGet(
+		keyword?: string,
+		page?: number,
+		search?: string,
+		size: number = 10,
+	): CancelablePromise<Page_Caregiver_> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/caregiver/list',
+			query: {
+				keyword: keyword,
+				page: page,
+				search: search,
+				size: size,
+			},
+		});
+	}
 
-    /**
-     * getCaregiverList
-     * @param keyword keyword
-     * @param page page
-     * @param search search
-     * @param size size
-     * @returns Page_Caregiver_ OK
-     * @throws ApiError
-     */
-    public static getCaregiverListUsingGet(
-keyword?: string,
-page?: number,
-search?: string,
-size: number = 10,
-): CancelablePromise<Page_Caregiver_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/caregiver/list',
-            query: {
-                'keyword': keyword,
-                'page': page,
-                'search': search,
-                'size': size,
-            },
-        });
-    }
+	/**
+	 * getStatisticsList
+	 * @returns Iterable_Caregiver_ OK
+	 * @throws ApiError
+	 */
+	public static getStatisticsListUsingGet(): CancelablePromise<Iterable_Caregiver_> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/caregiver/statistics',
+		});
+	}
 
-    /**
-     * getStatisticsList
-     * @returns Iterable_Caregiver_ OK
-     * @throws ApiError
-     */
-    public static getStatisticsListUsingGet(): CancelablePromise<Iterable_Caregiver_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/caregiver/statistics',
-        });
-    }
+	/**
+	 * updateCaregiver
+	 * @param id id
+	 * @returns Caregiver OK
+	 * @throws ApiError
+	 */
+	public static updateCaregiverUsingPut(id: number): CancelablePromise<Caregiver> {
+		return __request(OpenAPI, {
+			method: 'PUT',
+			url: '/caregiver/update',
+			body: id,
+		});
+	}
 
-    /**
-     * updateCaregiver
-     * @param id id
-     * @returns Caregiver OK
-     * @throws ApiError
-     */
-    public static updateCaregiverUsingPut(
-id: number,
-): CancelablePromise<Caregiver> {
-        return __request(OpenAPI, {
-            method: 'PUT',
-            url: '/caregiver/update',
-            body: id,
-        });
-    }
-
-    /**
-     * getWardCount
-     * @param city city
-     * @param ward ward
-     * @returns number OK
-     * @throws ApiError
-     */
-    public static getWardCountUsingGet(
-city: string,
-ward: string,
-): CancelablePromise<number> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/caregiver/wardCount',
-            query: {
-                'city': city,
-                'ward': ward,
-            },
-        });
-    }
-
+	/**
+	 * getWardCount
+	 * @param city city
+	 * @param ward ward
+	 * @returns number OK
+	 * @throws ApiError
+	 */
+	public static getWardCountUsingGet(city: string, ward: string): CancelablePromise<number> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/caregiver/wardCount',
+			query: {
+				city: city,
+				ward: ward,
+			},
+		});
+	}
 }
--- src/services/openApi/services/ConsultControllerService.ts
+++ src/services/openApi/services/ConsultControllerService.ts
@@ -9,369 +9,355 @@ import { OpenAPI } from '../core/OpenAPI';
 import { request as __request } from '../core/request';
 
 export class ConsultControllerService {
+	/**
+	 * saveConsult
+	 * @param consult consult
+	 * @returns Consult OK
+	 * @throws ApiError
+	 */
+	public static saveConsultUsingPost(consult: Consult): CancelablePromise<Consult> {
+		return __request(OpenAPI, {
+			method: 'POST',
+			url: '/consult',
+			body: consult,
+		});
+	}
 
-    /**
-     * saveConsult
-     * @param consult consult
-     * @returns Consult OK
-     * @throws ApiError
-     */
-    public static saveConsultUsingPost(
-consult: Consult,
-): CancelablePromise<Consult> {
-        return __request(OpenAPI, {
-            method: 'POST',
-            url: '/consult',
-            body: consult,
-        });
-    }
+	/**
+	 * deleteConsult
+	 * @param id id
+	 * @returns any OK
+	 * @throws ApiError
+	 */
+	public static deleteConsultUsingDelete(id: number): CancelablePromise<any> {
+		return __request(OpenAPI, {
+			method: 'DELETE',
+			url: '/consult',
+			query: {
+				id: id,
+			},
+		});
+	}
 
-    /**
-     * deleteConsult
-     * @param id id
-     * @returns any OK
-     * @throws ApiError
-     */
-    public static deleteConsultUsingDelete(
-id: number,
-): CancelablePromise<any> {
-        return __request(OpenAPI, {
-            method: 'DELETE',
-            url: '/consult',
-            query: {
-                'id': id,
-            },
-        });
-    }
+	/**
+	 * getHomepageReservation
+	 * @param state state
+	 * @returns number OK
+	 * @throws ApiError
+	 */
+	public static getHomepageReservationUsingGet(state: string): CancelablePromise<number> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/HRCount',
+			query: {
+				state: state,
+			},
+		});
+	}
 
-    /**
-     * getHomepageReservation
-     * @param state state
-     * @returns number OK
-     * @throws ApiError
-     */
-    public static getHomepageReservationUsingGet(
-state: string,
-): CancelablePromise<number> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/HRCount',
-            query: {
-                'state': state,
-            },
-        });
-    }
+	/**
+	 * getLongtimeCustomerList
+	 * @param endDate end_date
+	 * @param manager manager
+	 * @param startDate start_date
+	 * @param state state
+	 * @returns Consult OK
+	 * @throws ApiError
+	 */
+	public static getLongtimeCustomerListUsingGet(
+		endDate: string,
+		manager: string,
+		startDate: string,
+		state: string,
+	): CancelablePromise<Array<Consult>> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/Longtime',
+			query: {
+				end_date: endDate,
+				manager: manager,
+				start_date: startDate,
+				state: state,
+			},
+		});
+	}
 
-    /**
-     * getLongtimeCustomerList
-     * @param endDate end_date
-     * @param manager manager
-     * @param startDate start_date
-     * @param state state
-     * @returns Consult OK
-     * @throws ApiError
-     */
-    public static getLongtimeCustomerListUsingGet(
-endDate: string,
-manager: string,
-startDate: string,
-state: string,
-): CancelablePromise<Array<Consult>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/Longtime',
-            query: {
-                'end_date': endDate,
-                'manager': manager,
-                'start_date': startDate,
-                'state': state,
-            },
-        });
-    }
+	/**
+	 * getLongtimeMessageList
+	 * @param endDate end_date
+	 * @param manager manager
+	 * @param sendEndDate send_end_date
+	 * @param sendStartDate send_start_date
+	 * @param startDate start_date
+	 * @param state state
+	 * @param today today
+	 * @returns Consult OK
+	 * @throws ApiError
+	 */
+	public static getLongtimeMessageListUsingGet(
+		endDate: string,
+		manager: string,
+		sendEndDate: string,
+		sendStartDate: string,
+		startDate: string,
+		state: string,
+		today: string,
+	): CancelablePromise<Array<Consult>> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/LongtimeMessage',
+			query: {
+				end_date: endDate,
+				manager: manager,
+				send_end_date: sendEndDate,
+				send_start_date: sendStartDate,
+				start_date: startDate,
+				state: state,
+				today: today,
+			},
+		});
+	}
 
-    /**
-     * getLongtimeMessageList
-     * @param endDate end_date
-     * @param manager manager
-     * @param sendEndDate send_end_date
-     * @param sendStartDate send_start_date
-     * @param startDate start_date
-     * @param state state
-     * @param today today
-     * @returns Consult OK
-     * @throws ApiError
-     */
-    public static getLongtimeMessageListUsingGet(
-endDate: string,
-manager: string,
-sendEndDate: string,
-sendStartDate: string,
-startDate: string,
-state: string,
-today: string,
-): CancelablePromise<Array<Consult>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/LongtimeMessage',
-            query: {
-                'end_date': endDate,
-                'manager': manager,
-                'send_end_date': sendEndDate,
-                'send_start_date': sendStartDate,
-                'start_date': startDate,
-                'state': state,
-                'today': today,
-            },
-        });
-    }
+	/**
+	 * getMyPickList
+	 * @param managerName managerName
+	 * @returns Consult OK
+	 * @throws ApiError
+	 */
+	public static getMyPickListUsingGet(managerName: string): CancelablePromise<Array<Consult>> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/MyPick',
+			query: {
+				managerName: managerName,
+			},
+		});
+	}
 
-    /**
-     * getMyPickList
-     * @param managerName managerName
-     * @returns Consult OK
-     * @throws ApiError
-     */
-    public static getMyPickListUsingGet(
-managerName: string,
-): CancelablePromise<Array<Consult>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/MyPick',
-            query: {
-                'managerName': managerName,
-            },
-        });
-    }
+	/**
+	 * getPhoneConsult
+	 * @param phone phone
+	 * @returns Consult OK
+	 * @throws ApiError
+	 */
+	public static getPhoneConsultUsingGet(phone: string): CancelablePromise<Consult> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/Phone',
+			query: {
+				phone: phone,
+			},
+		});
+	}
 
-    /**
-     * getPhoneConsult
-     * @param phone phone
-     * @returns Consult OK
-     * @throws ApiError
-     */
-    public static getPhoneConsultUsingGet(
-phone: string,
-): CancelablePromise<Consult> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/Phone',
-            query: {
-                'phone': phone,
-            },
-        });
-    }
+	/**
+	 * getManagerRecallList
+	 * @param endDate end_date
+	 * @param manager manager
+	 * @param startDate start_date
+	 * @param state state
+	 * @returns Consult OK
+	 * @throws ApiError
+	 */
+	public static getManagerRecallListUsingGet(
+		endDate: string,
+		manager: string,
+		startDate: string,
+		state: string,
+	): CancelablePromise<Array<Consult>> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/RecallList',
+			query: {
+				end_date: endDate,
+				manager: manager,
+				start_date: startDate,
+				state: state,
+			},
+		});
+	}
 
-    /**
-     * getManagerRecallList
-     * @param endDate end_date
-     * @param manager manager
-     * @param startDate start_date
-     * @param state state
-     * @returns Consult OK
-     * @throws ApiError
-     */
-    public static getManagerRecallListUsingGet(
-endDate: string,
-manager: string,
-startDate: string,
-state: string,
-): CancelablePromise<Array<Consult>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/RecallList',
-            query: {
-                'end_date': endDate,
-                'manager': manager,
-                'start_date': startDate,
-                'state': state,
-            },
-        });
-    }
+	/**
+	 * getReservationList
+	 * @param manager manager
+	 * @param page page
+	 * @param size size
+	 * @returns Page_Consult_ OK
+	 * @throws ApiError
+	 */
+	public static getReservationListUsingGet(
+		manager: string,
+		page?: number,
+		size: number = 10,
+	): CancelablePromise<Page_Consult_> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/Reservation',
+			query: {
+				manager: manager,
+				page: page,
+				size: size,
+			},
+		});
+	}
 
-    /**
-     * getReservationList
-     * @param manager manager
-     * @param page page
-     * @param size size
-     * @returns Page_Consult_ OK
-     * @throws ApiError
-     */
-    public static getReservationListUsingGet(
-manager: string,
-page?: number,
-size: number = 10,
-): CancelablePromise<Page_Consult_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/Reservation',
-            query: {
-                'manager': manager,
-                'page': page,
-                'size': size,
-            },
-        });
-    }
+	/**
+	 * getSamePhoneConsult
+	 * @param phone phone
+	 * @returns boolean OK
+	 * @throws ApiError
+	 */
+	public static getSamePhoneConsultUsingGet(phone: string): CancelablePromise<boolean> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/SamePhone',
+			query: {
+				phone: phone,
+			},
+		});
+	}
 
-    /**
-     * getSamePhoneConsult
-     * @param phone phone
-     * @returns boolean OK
-     * @throws ApiError
-     */
-    public static getSamePhoneConsultUsingGet(
-phone: string,
-): CancelablePromise<boolean> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/SamePhone',
-            query: {
-                'phone': phone,
-            },
-        });
-    }
+	/**
+	 * getGoodByeCallConsult
+	 * @param page page
+	 * @param size size
+	 * @returns Page_Consult_ OK
+	 * @throws ApiError
+	 */
+	public static getGoodByeCallConsultUsingGet(
+		page?: number,
+		size: number = 10,
+	): CancelablePromise<Page_Consult_> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/goodbye',
+			query: {
+				page: page,
+				size: size,
+			},
+		});
+	}
 
-    /**
-     * getGoodByeCallConsult
-     * @param page page
-     * @param size size
-     * @returns Page_Consult_ OK
-     * @throws ApiError
-     */
-    public static getGoodByeCallConsultUsingGet(
-page?: number,
-size: number = 10,
-): CancelablePromise<Page_Consult_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/goodbye',
-            query: {
-                'page': page,
-                'size': size,
-            },
-        });
-    }
+	/**
+	 * getConsultList
+	 * @param from from
+	 * @param keyword keyword
+	 * @param page page
+	 * @param progress progress
+	 * @param search search
+	 * @param size size
+	 * @param state state
+	 * @param to to
+	 * @returns Page_Consult_ OK
+	 * @throws ApiError
+	 */
+	public static getConsultListUsingGet(
+		from?: string,
+		keyword?: string,
+		page?: number,
+		progress?: string,
+		search?: string,
+		size: number = 10,
+		state?: string,
+		to?: string,
+	): CancelablePromise<Page_Consult_> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/list',
+			query: {
+				from: from,
+				keyword: keyword,
+				page: page,
+				progress: progress,
+				search: search,
+				size: size,
+				state: state,
+				to: to,
+			},
+		});
+	}
 
-    /**
-     * getConsultList
-     * @param from from
-     * @param keyword keyword
-     * @param page page
-     * @param progress progress
-     * @param search search
-     * @param size size
-     * @param state state
-     * @param to to
-     * @returns Page_Consult_ OK
-     * @throws ApiError
-     */
-    public static getConsultListUsingGet(
-from?: string,
-keyword?: string,
-page?: number,
-progress?: string,
-search?: string,
-size: number = 10,
-state?: string,
-to?: string,
-): CancelablePromise<Page_Consult_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/list',
-            query: {
-                'from': from,
-                'keyword': keyword,
-                'page': page,
-                'progress': progress,
-                'search': search,
-                'size': size,
-                'state': state,
-                'to': to,
-            },
-        });
-    }
+	/**
+	 * getConsultAllList
+	 * @param from from
+	 * @param to to
+	 * @returns Consult OK
+	 * @throws ApiError
+	 */
+	public static getConsultAllListUsingGet(
+		from?: string,
+		to?: string,
+	): CancelablePromise<Array<Consult>> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/list/all',
+			query: {
+				from: from,
+				to: to,
+			},
+		});
+	}
 
-    /**
-     * getConsultAllList
-     * @param from from
-     * @param to to
-     * @returns Consult OK
-     * @throws ApiError
-     */
-    public static getConsultAllListUsingGet(
-from?: string,
-to?: string,
-): CancelablePromise<Array<Consult>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/list/all',
-            query: {
-                'from': from,
-                'to': to,
-            },
-        });
-    }
+	/**
+	 * getTargetConsultList
+	 * @param managerName managerName
+	 * @returns Consult OK
+	 * @throws ApiError
+	 */
+	public static getTargetConsultListUsingGet(
+		managerName: string,
+	): CancelablePromise<Array<Consult>> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/list/target',
+			query: {
+				managerName: managerName,
+			},
+		});
+	}
 
-    /**
-     * getTargetConsultList
-     * @param managerName managerName
-     * @returns Consult OK
-     * @throws ApiError
-     */
-    public static getTargetConsultListUsingGet(
-managerName: string,
-): CancelablePromise<Array<Consult>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/list/target',
-            query: {
-                'managerName': managerName,
-            },
-        });
-    }
+	/**
+	 * getProgressConsult
+	 * @param progress progress
+	 * @param page page
+	 * @param size size
+	 * @returns Page_Consult_ OK
+	 * @throws ApiError
+	 */
+	public static getProgressConsultUsingGet(
+		progress: string,
+		page?: number,
+		size: number = 10,
+	): CancelablePromise<Page_Consult_> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/progress',
+			query: {
+				page: page,
+				progress: progress,
+				size: size,
+			},
+		});
+	}
 
-    /**
-     * getProgressConsult
-     * @param progress progress
-     * @param page page
-     * @param size size
-     * @returns Page_Consult_ OK
-     * @throws ApiError
-     */
-    public static getProgressConsultUsingGet(
-progress: string,
-page?: number,
-size: number = 10,
-): CancelablePromise<Page_Consult_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/progress',
-            query: {
-                'page': page,
-                'progress': progress,
-                'size': size,
-            },
-        });
-    }
-
-    /**
-     * getDeleteReasonConsult
-     * @param page page
-     * @param size size
-     * @returns Page_Consult_ OK
-     * @throws ApiError
-     */
-    public static getDeleteReasonConsultUsingGet(
-page?: number,
-size: number = 10,
-): CancelablePromise<Page_Consult_> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/consult/reason',
-            query: {
-                'page': page,
-                'size': size,
-            },
-        });
-    }
-
+	/**
+	 * getDeleteReasonConsult
+	 * @param page page
+	 * @param size size
+	 * @returns Page_Consult_ OK
+	 * @throws ApiError
+	 */
+	public static getDeleteReasonConsultUsingGet(
+		page?: number,
+		size: number = 10,
+	): CancelablePromise<Page_Consult_> {
+		return __request(OpenAPI, {
+			method: 'GET',
+			url: '/consult/reason',
+			query: {
+				page: page,
+				size: size,
+			},
+		});
+	}
 }
